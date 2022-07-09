c109681cd895c0b0f5f449ff95f9fde36bbbc708
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-07-10 01:00:07 +0400

    JS started
    JS Started

---
+++ JigsawJS.js
@@ -0,0 +1,11 @@
+var r = 4, c = 4, currentTile, otherTile;
+
+window.onload = function(){
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        tile = document.createElement("img");
+        tile.src = "./images/5.jpg"
+        document.getElementById("grid").append(tile);
+    }
+}
