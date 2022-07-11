a9ac278040217ce44c55ac6af4bae4334c6aade4
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-07-11 15:20:45 +0400

    Update JigsawJS.js

--- JigsawJS.js
+++ JigsawJS.js
@@ -1,7 +1,8 @@
 var r = 4, c = 4, currentTile, otherTile;
 
 window.onload = function(){
-    for(i=0;i<r;i++)
+    for(i=0;i<r;i++)0524045757
+    
     {
         for(j=0;j<c;j++)
         tile = document.createElement("img");
