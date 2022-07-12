b69b5dcab7d7256bfc9a12f02d9620eb60983e71
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-07-13 02:59:26 +0400

    jigsaw complete
    puzzle complete styling good, database left leaderboard

--- JigsawJS.js
+++ JigsawJS.js
@@ -1,11 +1,118 @@
-var r = 4, c = 4, currentPuzzle, otherPuzzle, puzzle;
+var rows = 4;
+var columns = 4;
 
-window.onload = function(){
-    for(let i=0;i<r;i++)    
-    {
-        for(let j=0;j<c;j++)
-        puzzle = document.createElement("img");
-        puzzle.src = "./images/blank.jpg"
-        document.getElementById("grid").append(puzzle);
+var currTile;
+var otherTile;
+
+var turns = 0;
+var timeremaining = 60;
+var timerInterval;
+var userscore = 0;
+
+
+window.onload = function() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            //<img>
+            let tile = document.createElement("img");
+            tile.src = "./images/blank.jpg";
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart); //click on image to drag
+            tile.addEventListener("dragover", dragOver);   //drag an image
+            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+
+            document.getElementById("board").append(tile);
+        }
     }
+
+    //pieces
+    let pieces = [];
+    for (let i=1; i <= rows*columns; i++) {
+        pieces.push(i.toString()); //put "1" to "25" into the array (puzzle images names)
+    }
+    pieces.reverse();
+
+    for (let i = 0; i < pieces.length; i++) {
+        let tile = document.createElement("img");
+        tile.src = "./images/" + pieces[i] + ".jpg";
+
+        //DRAG FUNCTIONALITY
+        tile.addEventListener("dragstart", dragStart); //click on image to drag
+        tile.addEventListener("dragover", dragOver);   //drag an image
+        tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+        tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+        tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+        tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+
+        document.getElementById("pieces").append(tile);
+    }
+}
+
+//DRAG TILES
+function dragStart() {
+    currTile = this; //this refers to image that was clicked on for dragging
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to image that is being dropped on
+}
+
+function dragEnd() {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+}
+
+
+function starttimer()
+{
+    document.getElementById("startbutton").hidden = true;
+    document.getElementById("submitscore").hidden = false;
+    let timer = document.getElementById("timer");
+    timer.hidden = false;
+    timerInterval = setInterval(function(){
+        timeremaining -=1;
+        timer.innerHTML = "Time Remaining: "+ timeremaining;
+        if (timeremaining == 0)
+        {
+            clearInterval(timerInterval);
+        }
+    },1000) 
+}
+
+function scorecalculator()
+{
+    if(timeremaining==0)
+    {
+        submitscore.innerHTML = "Your time exceeded 60 seconds, Better Luck Next Time!"; 
+    }
+    else{
+        userscore = 3 * timeremaining;
+        submitscore.innerHTML = "CONGRATULATIONS! YOUR SCORE IS: "+userscore;
+    }
+}
+
+function verifypuzzle()
+{
+
 }
